86042cafab976f04c5217a9c5241bce0a8ab6747
hnandra <h_nandra@hotmail.co.uk>
2024-11-04 17:00:38 +0000

    Removed DocumentDBHelper
    URI generation no longer required with Azure Cosmos DB v3 SDK

--- NCS.DSS.ContentEnhancer/Cosmos/Helper/IDocumentDBHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NCS.DSS.ContentEnhancer.Cosmos.Helper
-{
-    public interface IDocumentDBHelper
-    {
-        Uri CreateDocumentCollectionUri();
-        Uri CreateDocumentUri(Guid? customerId);
-
-    }
-}
