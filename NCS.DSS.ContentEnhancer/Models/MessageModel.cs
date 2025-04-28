4ef057e4bc29c34a331988b379e5ec430fb35a4a
Pradeep Voorukonda <87078845+pvoorukonda-DFE@users.noreply.github.com>
2025-04-28 16:46:01 +0100

    Removed unused properties as well

--- NCS.DSS.ContentEnhancer/Models/MessageModel.cs
+++ NCS.DSS.ContentEnhancer/Models/MessageModel.cs
@@ -11,10 +11,5 @@ namespace NCS.DSS.ContentEnhancer.Models
         public bool IsNewCustomer { get; set; }
         public string TouchpointId { get; set; }
         public bool? DataCollections { get; set; }
-        public string EmailAddress { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string CurrentEmail { get; set; }
-        public string NewEmail { get; set; }
     }
 }
